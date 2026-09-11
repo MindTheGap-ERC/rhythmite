fe3ec460c8214a2b2743ed87f5fd53f61a228a7e
Hanno Spreeuw <h.spreeuw@esciencecenter.nl>
2026-09-11 17:22:28 +0200

    Pandas' read_csv has been updated.

--- plotOutput.py
+++ plotOutput.py
@@ -16,7 +16,7 @@ plotBench = False
 
 spatial_files = glob.glob('solution_t_*.ascii')
 for i in range(0,len(spatial_files)):
-    x = (pd.read_csv(spatial_files[i], delim_whitespace=True)).shift(axis=1).iloc[:,1:]
+    x = (pd.read_csv(spatial_files[i], sep=r"\s+")).shift(axis=1).iloc[:,1:]
     plotSpatial(x, spatial_files[i].rstrip('.ascii'), plotBench)
 
 
@@ -24,5 +24,5 @@ for i in range(0,len(spatial_files)):
 
 times_files = glob.glob('solution_x_*.ascii')
 for i in range(0,len(times_files)):
-    x = (pd.read_csv(times_files[i],delim_whitespace=True)).shift(axis=1).iloc[:,1:]
+    x = (pd.read_csv(times_files[i], sep=r"\s+")).shift(axis=1).iloc[:,1:]
     plotTemporal(x,times_files[i].rstrip('.ascii'))
